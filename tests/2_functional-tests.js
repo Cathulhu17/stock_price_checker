ea663bb00c7fe79a77a6c4c92ebf5dcfeaa762ae
Cathulhu17 <fontanella.luciano8@gmail.com>
2025-10-16 21:26:45 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,72 @@
-const chaiHttp = require('chai-http');
 const chai = require('chai');
+const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
+suite('Pruebas Funcionales', function () {
+  this.timeout(5000);
 
+  test('Ver una acción', function (done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'GOOG' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.property(res.body, 'stockData');
+        assert.property(res.body.stockData, 'stock');
+        assert.property(res.body.stockData, 'price');
+        assert.property(res.body.stockData, 'likes');
+        done();
+      });
+  });
+
+  test('Ver una acción y darle like', function (done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL', like: true })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isAtLeast(res.body.stockData.likes, 1);
+        done();
+      });
+  });
+
+  test('Ver la misma acción y volver a darle like (no debería duplicarse)', function (done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: 'AAPL', like: true })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isAtLeast(res.body.stockData.likes, 1);
+        done();
+      });
+  });
+
+  test('Ver dos acciones a la vez', function (done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'] })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.equal(res.body.stockData.length, 2);
+        assert.property(res.body.stockData[0], 'rel_likes');
+        done();
+      });
+  });
+
+  test('Ver dos acciones y darles like', function (done) {
+    chai.request(server)
+      .get('/api/stock-prices')
+      .query({ stock: ['GOOG', 'MSFT'], like: true })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body.stockData);
+        assert.equal(res.body.stockData.length, 2);
+        assert.property(res.body.stockData[0], 'rel_likes');
+        done();
+      });
+  });
 });
